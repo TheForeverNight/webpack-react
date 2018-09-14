0c79e31175d7b1482d8109e94adb51e65a13a14e
wangjch <wangjchm@yonyou.com>
2018-09-14 15:33:11 +0800

    install less,less-loader

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './form.less'
 
 class Form extends Component {
     render() {
         const {name,handleChange} = this.props;
         return (
-            <div>
+            <div className="form-wrap">
                 <input type="text" defaultValue={name} onChange={handleChange}/>
             </div>
         );
